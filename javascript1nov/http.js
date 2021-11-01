317fc0dfb9f0ad045096c1f7976f7dffeb074f5b
Tamojit Das <tamo.das.97@gmail.com>
2021-11-01 20:46:18 +0530

    js projects

--- javascript1nov/http.js
+++ javascript1nov/http.js
@@ -1,6 +1,6 @@
 //https://jsonplaceholder.typicode.com/todos/1
-let xhr = new XMLHttpRequest();
-xhr.open("get","https://jsonplaceholder.typicode.com/todos/1234");
+let xhr = new XMLHttpRequest();
+xhr.open("get", "https://jsonplaceholder.typicode.com/todos/1234");
 xhr.send();
 
 console.log(JSON.parse(xhr.response));
@@ -8,63 +8,57 @@ document.write(xhr.response);
 document.write("<pre>${xhr.response}</pre>");
 
 
-if(xhr.status==200)
-{
-	cosole.log(xhr.response);
-}
-else
-{
-console.log("Error ${xhr.status}");
+if (xhr.status === 200) {
+    cosole.log(xhr.response);
+} else {
+    console.log("Error ${xhr.status}");
 }
 
-xhr.onerror=()=>{
-console.log(xhr);
+xhr.onerror = () => {
+    console.log(xhr);
 }
 
-xhr.onload = function() {
-    alert(`Loaded: ${xhr.status} ${xhr.response}`);
+xhr.onload = function () {
+    alert(`Loaded:${xhr.status} ${xhr.response}`);
 };
-  
-xhr.onerror = function() { // only triggers if the request couldn't be made at all
-    alert(`Network Error`);
+
+xhr.onerror = function () {//only triggers if the request couldn't be made at all
+    alert(`Network Error`);
 };
-  
-xhr.onprogress = function(event) { // triggers periodically
-    alert(`Received ${event.loaded} of ${event.total}`);
+
+xhr.onprogress = function (event) { // triggers periodically
+    alert(`Received ${event.loaded} of ${event.total}`);
 };
 // =========================================
 
 
-
-let xhr = new XMLHttpRequest();
-xhr.open('GET', '/article/xmlhttprequest/example/load');
+let xhr = new XMLHttpRequest();
+xhr.open('GET', '/article/xmlhttprequest/example/load');
 xhr.send();
-xhr.onload = function() {
-  if (xhr.status != 200) alert(`Error ${xhr.status}: ${xhr.statusText}`); 
-  else alert(`Done, got ${xhr.response.length} bytes`);
+xhr.onload = function () {
+    if (xhr.status !== 200) alert(`Error ${xhr.status}: ${xhr.statusText}`);
+    else alert(`Done, got ${xhr.response.length} bytes`);
 };
-xhr.onprogress = function(event) {
-  if (event.lengthComputable) alert(`Received ${event.loaded} of ${event.total} bytes`);
-  else alert(`Received ${event.loaded} bytes`);
+xhr.onprogress = function (event) {
+    if (event.lengthComputable) alert(`Received ${event.loaded} of ${event.total} bytes`);
+    else alert(`Received ${event.loaded} bytes`);
 };
-xhr.onerror = function() {
-  alert("Request failed");
+xhr.onerror = function () {
+    alert("Request failed");
 };
 // =============================================
 
 
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "hello.txt", false);
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "hello.txt", false);
 xhr.send();
-document.write(xhr.response); // writes 'hello' to document
+document.write(xhr.response); // writes 'hello' to document
 
 
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "hello.txt");
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "hello.txt");
 xhr.send();
-document.write(xhr.response); // empty string
+document.write(xhr.response); // empty string
 // ======================================
 
 
